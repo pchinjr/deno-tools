32025bb45e850ae7664cabdc93889b4eebcea4c7
Paul Chin Jr <pchinjr@gmail.com>
2020-05-22 09:38:50 -0400

    update

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,7 +1,7 @@
 
 export async function handler(req: object) { 
+  console.log(req)
   return {
-    statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
